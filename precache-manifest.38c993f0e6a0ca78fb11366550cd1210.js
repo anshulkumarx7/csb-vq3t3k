7372e112afcf4f66ea36ebbb363aa21790270a7d
Code Sandbox <hello@codesandbox.io>
2023-01-25 14:46:11 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.38c993f0e6a0ca78fb11366550cd1210.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79054a82c0e390892575df68aa68d81",
+    "url": "https://anshulkumarx7.github.io/csb-vq3t3k/index.html"
+  },
+  {
+    "revision": "4bb34cee6e36e0afa3a0",
+    "url": "https://anshulkumarx7.github.io/csb-vq3t3k/static/js/2.c7dae802.chunk.js"
+  },
+  {
+    "revision": "a3a83f33602a112f9b65",
+    "url": "https://anshulkumarx7.github.io/csb-vq3t3k/static/js/main.82929c46.chunk.js"
+  },
+  {
+    "revision": "05f922076ade5bf55649",
+    "url": "https://anshulkumarx7.github.io/csb-vq3t3k/static/js/runtime-main.ac8afee6.js"
+  }
+]);
---
+++ static/js/main.82929c46.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-forms"]=this["webpackJsonpreact-forms"]||[]).push([[0],{3:function(e,t,n){e.exports=n(9)},9:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),c=n(2);var l=function(){return r.a.createElement("div",{className:"container"},r.a.createElement("h1",null,"Hello "),r.a.createElement("input",{type:"text",placeholder:"What's your name?"}),r.a.createElement("button",null,"Submit"))};n.n(c).a.render(r.a.createElement(l,null),document.getElementById("root"))}},[[3,1,2]]]);
+//# sourceMappingURL=main.82929c46.chunk.js.map
